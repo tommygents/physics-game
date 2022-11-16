30e8005035ef0baf327db4c9b19a3d044263c1bb
tommygents <tmaranges+gh@gmail.com>
2022-11-16 12:03:16 -0500

    began making animal spawner and raining animals scene

---
+++ Assets/Scripts/AnimalSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+//the goal for this script is to use object-pooling to 
+
+
+}
