24268ea44fc0633b91151c763431757d9ba4cbce
diane2007 <wangruochong0406@gmail.com>
2022-11-19 21:34:48 -0500

    Updated the look of the ark. Finish the progress bar and its animation. The island is up, too.

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DentedPixel;
 
 public class ProgressBar : MonoBehaviour
 {
+    public GameObject Bar;
+    public int time;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AnimateBar();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AnimateBar()
+    {
+        LeanTween.scaleX(Bar, 1, time);
     }
 }
