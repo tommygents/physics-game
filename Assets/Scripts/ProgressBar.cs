0f5873898b085d95b2032016b4005078c119a8cf
diane2007 <wangruochong0406@gmail.com>
2022-12-08 21:59:00 -0500

    no message

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -14,12 +14,6 @@ public class ProgressBar : MonoBehaviour
         AnimateBar();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void AnimateBar()
     {
         LeanTween.scaleX(Bar, 1, time);
